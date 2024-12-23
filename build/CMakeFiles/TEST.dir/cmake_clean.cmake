3a9e9ead9596d7c4a91127acedadebe5c65d014c
gaobin4216 <gaobin4216@gmail.com>
2024-12-24 01:49:47 +0800

    ceshi

---
+++ build/CMakeFiles/TEST.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEST.dir/src/main.cpp.obj"
+  "CMakeFiles/TEST.dir/src/main.cpp.obj.d"
+  "TEST.exe"
+  "TEST.exe.manifest"
+  "TEST.pdb"
+  "libTEST.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
